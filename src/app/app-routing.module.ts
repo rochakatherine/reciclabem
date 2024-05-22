ac26d87d614601d66e32b79cdd968092382e3634
rochakatherine <sanrochakatherine@gmail.com>
2024-05-21 21:17:10 -0300

    feat: update website recicla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'home', loadChildren: () => import('./features/sobre/sobre.module').then(m => m.HomeModule) },
   // Adicione outras rotas conforme necessário
 ];
 
